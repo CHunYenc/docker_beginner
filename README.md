97f138f8f572a7ce9c6b19731007aa53229f7681
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2021-05-17 22:58:04 +0800

    feat: flask-redis build is ok, 使用 oracle vm 操作.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # docker_beginner
+
+1. git clone project
+2. cd flask_hello_docker
+3. python3 -m venv venv
+4. source venv/bin/activate
+
+## docker 小撇步
+
+1. docker images == docker image ls
+2. docker run `-d` `--name` redis redis (`-d 背景執行 --name dockername`)
+
+## Linux 小技巧
+
+### awk 搜尋
+
+```
+docker container ls -f status=exited | awk '{print$1}' | awk 'NR>1' | xargs docker container rm
+```
---
+++ flask_redis/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from redis import Redis
+import os
+import socket
+
+app = Flask(__name__)
+redis = Redis(host="redis", port=6379)
+
+
+@app.route("/")
+def index():
+    redis.incr("hits")
+    return f'Hello Container World! I have been {redis.get("hits")} times and my hostname is {socket.gethostname()}'
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
